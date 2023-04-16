1123956e7426065addd39a65c3debfec3a8ca13a
bibil2002 <maks.konanyhin@mail.ru>
2023-04-16 17:43:45 +0300

    существенно переработана механика, теперь шары движутся в определенном направлении и их количество можно ограничить.Но в этой версии нет возможности взаимодействовать с шарами

--- game.py
+++ game.py
@@ -16,29 +16,60 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-
+balls =[]
 counter = 0 # счетчик очков
 
-def new_ball():
+def new_ball(x, y, r, delta_x, delta_y, color):
     '''
-    Рисует круг в случайном месте
+    добавляет новый круг
+    :param x:  координата центра
+    :param y: координата центра
+    :param r: радиус
+    следующие числа заменяют угол напрвления движения
+    :param delta_x: изменение координаты x за обновление экрана
+    :param delta_y: изменение координаты y за обновление экрана
+    :param color: цвет круга
     '''
-    global x, y, r
-    x = randint(100,700)
-    y = randint(100,500)
-    r = randint(30,50)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+
+    global balls
+
+    balls.append({'x':x, 'y': y, 'r':r, 'delta_x':delta_x, 'delta_y':delta_y, 'color':color})
 
 def click(event):
     '''
-     определяет попал ли клик в круг или нет
-    :return: 1 в случае попадания, -1 иначе
+    будет удалять круги в которые попал щелчком и считать очки или
+    это будет делать функция processing_balls, пока не решил
     '''
-    if ((event.pos[0] - x)**2 + (event.pos[1] - y)**2)**0.5 <= r:
-        return 1
-    else:
-        return -1
+    pass
+def processing_balls():
+    '''
+    отвечает за движение кругов.И случайным образом изменеят напрвление
+    движения когда центр круга выходит за рамки экрана.
+    '''
+
+
+
+
+    for i in range(len(balls)):
+        balls[i]['x'] += balls[i]['delta_x']
+        balls[i]['y'] += balls[i]['delta_y']
+        circle(screen, balls[i]['color'], (balls[i]['x'], balls[i]['y']), balls[i]['r'])
+        if balls[i]['x'] > 1200:
+            balls[i]['delta_x'] = randint(-50, -10)
+            balls[i]['delta_y'] = randint(-50, 50)
+
+        elif balls[i]['x'] < 0:
+            balls[i]['delta_x'] = randint(10, 50)
+            balls[i]['delta_y'] = randint(-50, 50)
+
+        if balls[i]['y'] > 900:
+            balls[i]['delta_x'] = randint(-50, 50)
+            balls[i]['delta_y'] = randint(-50, 0)
+        elif balls[i]['y'] < 0:
+            balls[i]['delta_x'] = randint(-50, 50)
+            balls[i]['delta_y'] = randint(0, 50)
+
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -51,9 +82,22 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            counter += click(event)
-            print(counter)
-    new_ball()
+            click(event)
+
+    processing_balls()
+
+
+    if len(balls) <= 2: # ограничение числа кругов
+        new_ball( x = randint(100,700),
+                  y = randint(100,500),
+                  r = randint(30,50),
+                  delta_x = randint(-50,50),
+                  delta_y = randint(-50,50),
+                  color = COLORS[randint(0, 5)])
+
+
+
+
     pygame.display.update()
     screen.fill(BLACK)
 
